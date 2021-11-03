cc1bcadcfb0178dc335f1c84c13553b95d15f0a1
jonghyun <heiwais25@gmail.com>
2021-11-04 00:40:41 +0900

    Add shadow to the row item

--- components/home/MainRowItem.tsx
+++ components/home/MainRowItem.tsx
@@ -6,7 +6,7 @@ type IProps = {
 
 const MainRowItem: React.FC<IProps> = ({ title, children }) => {
   return (
-    <div className="w-[480px] border p-2 space-y-2">
+    <div className="w-[480px] border p-2 space-y-2 shadow-md">
       <div className="text-xl border-b p-2">{title}</div>
       {children}
     </div>
